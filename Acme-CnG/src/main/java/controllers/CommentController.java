0b1c4d206939805a0c0e7e9f478b3073b600e948
danhidsan <danhidsan@alum.us.es>
2017-03-21 19:53:29 +0100

    Customer can't display banned comments

--- Acme-CnG/src/main/java/controllers/CommentController.java
+++ Acme-CnG/src/main/java/controllers/CommentController.java
@@ -1,6 +1,7 @@
 
 package controllers;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 import javax.validation.Valid;
@@ -26,23 +27,27 @@ public class CommentController extends AbstractController {
 	// Services ------------------------------------------------
 
 	@Autowired
-	private CommentService	commentService;
+	private CommentService				commentService;
 
 	@Autowired
 	private CommentableEntityService	commentableEntityService;
 
-	
 	@Autowired
-	private ActorService	actorService;
+	private ActorService				actorService;
 
-	
+
 	@RequestMapping(value = "/listComments", method = RequestMethod.GET)
-	public ModelAndView listListMyApplications(@RequestParam int commentableEntityId) {
+	public ModelAndView listComments(@RequestParam final int commentableEntityId) {
 		ModelAndView result;
 		Collection<Comment> comments;
-		CommentableEntity c = this.commentableEntityService.findOneAlternativo(commentableEntityId);
-		comments = c.getCommentsReceived();
-		
+		final CommentableEntity c = this.commentableEntityService.findOneAlternativo(commentableEntityId);
+
+		comments = new ArrayList<Comment>();
+
+		for (final Comment co : c.getCommentsReceived())
+			if (co.isBanned() == false)
+				comments.add(co);
+
 		result = new ModelAndView("comment/listComments");
 		result.addObject("comments", comments);
 		result.addObject("commentable", c.getId());
@@ -50,12 +55,11 @@ public class CommentController extends AbstractController {
 		return result;
 	}
 
-
 	@RequestMapping(value = "/create", method = RequestMethod.GET)
 	public ModelAndView createComment(@RequestParam final int commentableEntityId) {
 		ModelAndView result;
 		Comment comment;
-		
+
 		comment = this.commentService.create(commentableEntityId);
 
 		result = this.createEditModelAndView(comment);
@@ -77,8 +81,6 @@ public class CommentController extends AbstractController {
 			}
 		return result;
 	}
-	
-	
 
 	// Ancillary methods ---------------------------------------------------------
 	protected ModelAndView createEditModelAndView(final Comment comment) {
