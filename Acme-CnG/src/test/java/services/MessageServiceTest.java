9af6fb5a76553f9959dc4bbd2f6b942725e21623
adrluqmon <adrluqmon@alum.us.es>
2017-03-25 17:28:36 +0100

    Añadido la mitad de mis test

--- Acme-CnG/src/test/java/services/MessageServiceTest.java
+++ Acme-CnG/src/test/java/services/MessageServiceTest.java
@@ -1,15 +1,20 @@
 
 package services;
 
+import java.util.Collection;
+
 import javax.transaction.Transactional;
+import javax.validation.ConstraintViolationException;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.util.Assert;
 
 import utilities.AbstractTest;
+import domain.Actor;
 import domain.PrivateMessage;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -24,8 +29,14 @@ public class MessageServiceTest extends AbstractTest {
 	@Autowired
 	private PrivateMessageService	privateMessageService;
 
+	@Autowired
+	private ActorService			actorService;
 
-	protected void template(final String username, final int messageId, final Class<?> expected) {
+
+	//Customers = 98,99,100
+	//Admin = 97
+
+	protected void template(final String username, final int enviarId, final int recibirId, final Class<?> expected) {
 
 		Class<?> caught;
 
@@ -34,11 +45,28 @@ public class MessageServiceTest extends AbstractTest {
 
 			this.authenticate(username);
 
-			final PrivateMessage pm = this.privateMessageService.findOne(messageId);
+			final Actor envia = this.actorService.findOneToSent(enviarId);
+			final Actor recibe = this.actorService.findOneToSent(recibirId);
 
-			pm.setText("holaaaaaa");
+			final Collection<PrivateMessage> enviadosInicial = envia.getSendedMessages();
+			final int numeroDeMensajesEnviados = enviadosInicial.size();
+
+			final Collection<PrivateMessage> recibidosInicial = recibe.getRecivedMessages();
+			final int numeroDeMensajesRecibidos = recibidosInicial.size();
+
+			final PrivateMessage pm = this.privateMessageService.create();
+
+			pm.setAttachments("");
+			pm.setCopy(true);
+			pm.setRecipient(recibe);
+			pm.setSender(envia);
+			pm.setText("Me gustaria asistir mañana dia 15 a las 9:00");
+			pm.setTitle("Tutoria");
 
 			this.privateMessageService.save(pm);
+
+			Assert.isTrue(envia.getSendedMessages().size() == numeroDeMensajesEnviados + 1);
+			Assert.isTrue(recibe.getRecivedMessages().size() == numeroDeMensajesRecibidos + 1);
 
 			this.unauthenticate();
 			this.privateMessageService.flush();
@@ -54,16 +82,98 @@ public class MessageServiceTest extends AbstractTest {
 	public void driver() {
 
 		final Object testingData[][] = {
-			{
-				"customer1", 106, null
+			{   //customer 1 envia mensaje correcto a customer 2
+				"customer1", 98, 99, null
 			}, {
-				"customer2", 107, null
+				//customer 2 enviar mensaje correcto a customer 1
+				"customer2", 99, 98, null
+			}, {
+				//customer 3 envia mensaje correcto a customer 1
+				"customer3", 100, 98, null
+			}, {
+				//administrador envia mensaje correcto a customer 3
+				"admin", 97, 100, null
+			}, {
+				//Simular post hacking. Logeado como customer3, pero en realidad soy customer 2 intentando enviar mensaje a customer 1
+				"customer3", 99, 98, IllegalArgumentException.class
+			}, {
+				//Usuario no autenticado intenta enviar mensaje a otro actor.
+				null, 98, 99, IllegalArgumentException.class
+			}, {
+				//Customer 1 intenta enviar a un customer o admin que no existe un mensaje
+				"customer1", 98, 1000, IllegalArgumentException.class
 			}
 		};
 
 		for (int i = 0; i < testingData.length; i++)
-			this.template((String) testingData[i][0], (int) testingData[i][1], (Class<?>) testingData[i][2]);
+			this.template((String) testingData[i][0], (int) testingData[i][1], (int) testingData[i][2], (Class<?>) testingData[i][3]);
 
 	}
 
+	protected void template2(final String username, final int enviarId, final int recibirId, final int opcionId, final Class<?> expected) {
+
+		Class<?> caught;
+
+		caught = null;
+		try {
+
+			this.authenticate(username);
+
+			final Actor envia = this.actorService.findOneToSent(enviarId);
+			final Actor recibe = this.actorService.findOneToSent(recibirId);
+
+			final Collection<PrivateMessage> enviadosInicial = envia.getSendedMessages();
+			final int numeroDeMensajesEnviados = enviadosInicial.size();
+
+			final Collection<PrivateMessage> recibidosInicial = recibe.getRecivedMessages();
+			final int numeroDeMensajesRecibidos = recibidosInicial.size();
+
+			final PrivateMessage pm = this.privateMessageService.create();
+
+			if (opcionId == 1) {
+				pm.setAttachments("");
+				pm.setCopy(true);
+				pm.setRecipient(recibe);
+				pm.setSender(envia);
+				pm.setText("Me gustaria asistir mañana dia 15 a las 9:00");
+
+			}
+			if (opcionId == 2) {
+				pm.setAttachments("");
+				pm.setCopy(true);
+				pm.setRecipient(recibe);
+				pm.setSender(envia);
+				pm.setTitle("Reunion de mañana");
+
+			}
+
+			this.privateMessageService.save(pm);
+
+			this.unauthenticate();
+			this.privateMessageService.flush();
+
+		} catch (final Throwable oops) {
+			caught = oops.getClass();
+		}
+
+		this.checkExceptions(expected, caught);
+
+	}
+	@Test
+	public void driver2() {
+
+		final Object testingData[][] = {
+			{   //customer 1 envia mensaje sin titulo a customer 2
+				"customer1", 98, 99, 1, ConstraintViolationException.class
+			}, {
+				//customer 2 enviar mensaje sin texto a customer 1
+				"customer2", 99, 98, 2, ConstraintViolationException.class
+			}
+
+		};
+
+		for (int i = 0; i < testingData.length; i++)
+			this.template2((String) testingData[i][0], (int) testingData[i][1], (int) testingData[i][2], (int) testingData[i][3], (Class<?>) testingData[i][4]);
+
+	}
 }
